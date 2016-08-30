6ad26230d1963c1ee4cf4306949d5052cca330fd
bkalish <bkalish@gmail.com>
2016-08-30 15:43:44 -0400

    v1.2.1 - awesome items now laid out with css flexbox

--- templates/archive-awesomebox.php
+++ templates/archive-awesomebox.php
@@ -10,7 +10,7 @@ $helper = new Awesomebox_Helper();
 get_header();
 ?>
 <div id="main">
-  <div id="container_wrap" class="container-pagewithposts equal_height right-1-col">
+  <div id="container_wrap" class="container-pagewithposts equal_height">
     <?php if (function_exists('weaverii_get_paginate_archive_page_links')): ?>
       <div id="infobar">
         <span class='infobar_right'>
@@ -26,16 +26,18 @@ get_header();
       <h1 class="entry-title">
         <?php echo $helper->get_title(); ?>
       </h1>
-      <?php if ( have_posts() ): ?>
-        <?php while ( have_posts() ): ?>
-          <?php
-          the_post();
-          load_template( dirname( __FILE__ ) . '/content-awesomebox.php', False );
-          ?>
-        <?php endwhile; ?>
-      <?php else: ?>
-        <?php echo __('Nothing found'); ?>
-      <?php endif; ?>
+      <div id="awesomebox-content">
+        <?php if ( have_posts() ): ?>
+          <?php while ( have_posts() ): ?>
+            <?php
+            the_post();
+            load_template( dirname( __FILE__ ) . '/content-awesomebox.php', False );
+            ?>
+          <?php endwhile; ?>
+        <?php else: ?>
+          <?php echo __('Nothing found'); ?>
+        <?php endif; ?>
+      </div>
       <?php if (function_exists('weaverii_get_paginate_archive_page_links')): ?>
         <nav id="nav-below">
           <?php echo weaverii_get_paginate_archive_page_links( 'plain', 2, 2 ); ?>
